5a037768a2f566a43b6adbb7924ea37b44523736
Your Name <razzrahul789@gmail.com>
2024-03-30 15:33:39 +0530

    jesus

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,7 +8,10 @@ const user=require("../controller/userController")
 
 router.post("/commonregister",tryCatchMiddleware(user.commonRegister))
 router.post("/commonlogin",tryCatchMiddleware(user.commonlogin))
-router.get("/getallevents",tryCatchMiddleware,(user.getAllEvents))
+router.get("/getallevents",tryCatchMiddleware(user.getAllEvents))
+router.post('/paymentstart',tryCatchMiddleware(user.paymentInit))
+router.post('/paymentfinal/:id/:user',tryCatchMiddleware(user.verifyPayment))
+router.get('/fetchallbooking',tryCatchMiddleware(user.getAllBookings))
 
 
 
